7d87ab612fb474176ce433d7cf47b40e090a7460
MasonV <masonverkruisen@gmail.com>
2018-06-15 20:56:55 -0400

    Final Upload
    This is backup of the files running on a raspberry pi to keep r/stevedangle updating. I had thought to make it more object oriented but its been running for months and refactoring seems like a misuse of time when there are other projects to work on.

--- SDP.py
+++ SDP.py
@@ -9,37 +9,32 @@ import requests
 
 import pprint
 
-from keys import *
+
 
 class User:
-	def __init__(self,platform,**kwargs):
+	def __init__(self,platform):
 		# creates a base user with the specified platform 
 		# and if they are a developer it'll take a dict of 
 		# key word arguments for secrets, keys, passwords.
 		self.platform = platform
 		self.isDev = False
-		if kwargs is not None:
-			self.isDev = True
-			for key,val in kwargs.items():
-				# print("Key is : " + key + ", with a value of: " + val)
-				setattr(self,key,val)
 	def is_dev(self):
 		#Returns whether the user is a developer or not
 		return self.isDev
 
 class RedditDev(User):
-	def __init__(self,**kwargs):
+	isDev = True
+	def __init__(self):
 		#Creates a reddit developer instance. Requries username, password, client id, client secret, & user agent
-		User.__init__(self,'Reddit',**kwargs)
-		self.isDev = True
+		User.__init__(self,'Reddit')
 	def establish_api_connection(self):
-		reddit_api = praw.Reddit(client_id=self.client_id,client_secret=self.client_secret,user_agent=self.user_agent,username=self.username,password=self.password)
-		return reddit_api
+		self.reddit_api = praw.Reddit(client_id="",client_secret="",user_agent="",username="",password="")
+		return self.reddit_api
 	def create_post(self,api,post_type,Text_or_URL,title,subreddit,stickey=False,sort='new'):
 		if post_type == "text":
-			thread = reddit_connect.subreddit(subreddit).submit(title,selftext=selftext)
+			thread = self.reddit_api.subreddit(subreddit).submit(title,selftext=Text_or_URL)
 		elif post_type == "link":
-			thread = reddit_connect.subreddit(subreddit).submit(title,url=text_or_URL)
+			thread = self.reddit_api.subreddit(subreddit).submit(title,url=Text_or_URL)
 		if stickey:
 			thread.mod.sticky()
 		if sort != "new":
@@ -47,113 +42,93 @@ class RedditDev(User):
 		return thread
 
 
-def getStringAfterString(search,str1,str2,x=1):
-	#find the first string, then search for the xth instance of the 2nd
-	#required libraries
-	#	re
-	search = search[search.find(str1):]
-	index = [m.start() for m in re.finditer(search,str2)][0] #index 2 is third occurrence 
-	string = search[index:index+len(str2)]
-	
-	return String
-	
-# Creating Instance of Twitter Users, and connect to twitter through API
-# twitter_user = TwitterDev('MacerV', 'tSpHLSbHtohFnNhkm4THqRITP', 'PAExPZI5GYDR6KQWZc5Ac9QxUFUPF2nLkmDXhhjQOwfmCFQotf','977625156-rE3KQ1VB4nk4yD7AxbHu8AvNVLXgjkkmf8OJ6Osm', 'Y68yLYZG26gZ90sKK3CAIXo33xCrasP7lbHUmtApI2dJB')
-# twitter_target = TwitterUser('JesseBlake')
-# twitter_connect = twitter_user.establish_api_connection()
-reddit_user = RedditDev(**redditkeys)
-reddit_connect = reddit_user.establish_api_connection()
 
-# Itunes SDP
-## Itunes API is shit and doesn't work for podcasts like at all
-# https://itunes.apple.com/ca/podcast/steve-dangle-podcast/id669828195?mt=2#
+def main():	
+	currentTime = datetime.datetime.now()
+	print(currentTime, end='')  # does sameline print for next statement
+	print(": Script started.")
+	# Creating Instance of Twitter Users, and connect to twitter through API
+	reddit_user = RedditDev()
+	reddit_connect = reddit_user.establish_api_connection()
+	print("Connected to Reddit")
 
-
-
-
-#Sound Cloud search
-SoundCloud = 'https://soundcloud.com/steve-dangle-podcast'
-page = requests.get(SoundCloud)
-html = page.text
-text = html.encode('utf-8')
-TrackLocation = text.find("itemprop=\"track\"")
-StreamLocation = text.find("itemprop=\"url\"",TrackLocation)
-Href =  text.find("href=\"",StreamLocation)
-EndHref = text.find("\"",Href+6)
-PodcastLink = text[Href+6:EndHref]
-FullSCLink = "https://soundcloud.com" + PodcastLink
-print("Most Recent Podcast Link: " + FullSCLink)
-	
-
-#reads soundcloud 
-url = 'http://feeds.feedburner.com/stevedanglepodcast'
-page = feedparser.parse(url)
-
-
-
-podcastTitle = unicodedata.normalize('NFKD', page.entries[0]["title"]).encode('ascii','ignore')
-publishedDate = unicodedata.normalize('NFKD', page.entries[0]["published"]).encode('ascii','ignore')
-publishedDate = publishedDate[:len(publishedDate)-len(' +0000')]
-
-newestPodcast = datetime.datetime.strptime(publishedDate, '%a, %d %b %Y %H:%M:%S')
-
-#Starting to ad-hoc this a bit. Opens the ics calendar file and just checks to see if there is a game
-file_name = "C:\Users\Mason\Documents\Projects\SDP\lastchecked.txt"
-
-with open(file_name) as file_Object:
-	lastPodcast = file_Object.read()	
-file_Object.closed
-
-if lastPodcast != publishedDate:
-	# New Podcast is out!!!
-	print("This is a new podcast.")
-	with open(file_name, 'w') as file_Object:
-		 file_Object.write(publishedDate)
+	#grabs last registered podcast
+	file_name = "lastchecked.txt"
+	with open(file_name) as file_Object:
+		print("Reading Last Podcast Date")
+		lastPodcast = file_Object.read()	
 	file_Object.closed
+
+	#Over a 8 hour period it will repeatedly check if there's a new more recent podcast.
+	# breaks if a new one is found or 8 hours has passed. 
+	url = 'http://feeds.feedburner.com/stevedanglepodcast'
+	posting_window = 16 #time in hours
+	max_Sleep = posting_window*60*60
+	while True:
+		print("Reading feed until th end of the day or new podcast is found.")
+		page = feedparser.parse(url)
+		podcastTitle = unicodedata.normalize('NFKD', page.entries[0]["title"])
+		publishedDate = unicodedata.normalize('NFKD', page.entries[0]["published"])
+		publishedDate = publishedDate[:len(publishedDate)-len(' +0000')]
+		# newestPodcast = datetime.datetime.strptime(publishedDate, '%a, %d %b %Y %H:%M:%S')
+		
+		print(podcastTitle)
+
+		if lastPodcast == publishedDate:
+			if max_Sleep > 0:
+				print("No new podcast, sleeping for 15 minutes")
+				time.sleep(15*60)
+				max_Sleep = max_Sleep - 15*60
+			else:
+				break
+		else:
+			break
+
+	print("Podcast Feed Analyzed")
+	print("Last Registered Podcast: " + lastPodcast)
+	print("Most Recent Podcast: " + publishedDate)
+
+
+	#if the last registered podcast is not the same as the last podcast then we'll have a new podcast!
+	if lastPodcast != publishedDate:
+		# New Podcast is out!!!
+		print("This is a new podcast.")
+	
+		with open(file_name, 'w') as file_Object:
+			file_Object.write(publishedDate)
+		file_Object.closed
+	
+		#Grabbing the podcast link from SoundCloud
+		SoundCloud = 'https://soundcloud.com/steve-dangle-podcast'
+		page = requests.get(SoundCloud)
+		html = page.text
+		text = html
+		TrackLocation = text.find("itemprop=\"track\"")
+		StreamLocation = text.find("itemprop=\"url\"",TrackLocation)
+		Href =  text.find("href=\"",StreamLocation)
+		EndHref = text.find("\"",Href+6)
+		PodcastLink = text[Href+6:EndHref]
+		FullSCLink = "https://soundcloud.com" + PodcastLink
+		print("Most Recent Podcast Link: " + FullSCLink)
 	
 	
-# Jesse_tweets = twitter_target.get_tweets(twitter_connect,since_id=lastTweetID)
-# # Jesse_tweets = twitter_target.get_tweets(twitter_connect,num_of_tweets=10)
-
+		#The Podcast Link Text	
+		SoundCloud = "[SoundCloud](https://soundcloud.com/steve-dangle-podcast) \n\n"
+		ITunes = "[Itunes](https://itunes.apple.com/ca/podcast/steve-dangle-podcast/id669828195?mt=2) \n\n"
+		Youtube = "[Youtube](https://www.youtube.com/channel/UC0a0z05HiddEn7k6OGnDprg/videos) \n\n"
 	
-# #Looks for the first instance of a podcast tweet and stops (so no triple posting by accident
-# for tweet in Jesse_tweets:
-	# if tweet.text.find("Pod")>-1:
-		# # print("FOUND A PODCAST MOTHERFUCKER!")
-		# try:
-			# podcastlink = tweet.text[tweet.text.find("https"):tweet.text.find("https")+23]
-			
-			# selftext = "New Steve Dangle Podcast \n "
-			# title = "Steve Dangle Podcast - " + d.datetime.strftime('%b %d, %Y')
-			# subreddit = "stevedangle"
-			
-			# print(title)
-			
-			# # podcastpost = RedditDev.create_post(reddit_connect,"text",selftext,title,subreddit,True,"new")
-			# break
+		selftext = "New Steve Dangle Podcast : " + podcastTitle + "\n\nLinks:\n\n" + SoundCloud + ITunes + Youtube
+		# print(selftext) 		
+		
+		title = "The Steve Dangle Podcast - " + podcastTitle
+		subreddit = "stevedangle"
 
-		# except:
-			# print("Must not be a podcast")
+		print(reddit_connect)
+		print(selftext+"\n"+title+"\n"+subreddit)
+					
+		podcastpost = reddit_user.create_post(reddit_connect,"text",selftext,title,subreddit,True,"new")
 
-# with open(file_name,'w') as file_Object:
-	# lastTweetID = Jesse_tweets[1].id
-	# file_Object.write(lastTweetID)
-# file_Object.closed
-
-
-	SoundCloud = "[SoundCloud](https://soundcloud.com/steve-dangle-podcast) \n"
-	ITunes = "[Itunes](https://itunes.apple.com/ca/podcast/steve-dangle-podcast/id669828195?mt=2) \n"
-	Youtube = "[Youtube](https://www.youtube.com/channel/UC0a0z05HiddEn7k6OGnDprg/videos) \n"
-
-	selftext = "New Steve Dangle Podcast : " + podcastTitle + "\nLinks:\n" + SoundCloud + ITunes + Youtube
-	# print(selftext) 
-	
-	
-	title = "The Steve Dangle Podcast - " + podcastTitle
-	subreddit = "stevedangle"
-						
-	podcastpost = reddit_user.create_post(reddit_connect,"text",selftext,title,subreddit,True,"new")
-
-
+#script start
+main()
 
 
--- keys - Empty.py
+++
@@ -1,9 +0,0 @@
-#Insert Reddit keys
-redditkeys = { 
-	#"keyname": "key"
-	"username": "",
-	"password": "",
-	"user_agent": "",
-	"client_id": "",
-	"client_secret": "",
-}
